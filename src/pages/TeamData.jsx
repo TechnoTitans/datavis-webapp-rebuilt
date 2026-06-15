b7565fd4287de198857af2761bffed6203d88b07
Jiayu Wang <jiayuw12@gmail.com>
2026-06-15 12:43:36 -0400

    a

--- src/pages/TeamData.jsx
+++ src/pages/TeamData.jsx
@@ -15,7 +15,7 @@ function TeamData() {
     const teamStr = String(teamNumber)
     setSelectedTeams(prev => {
       const prevArray = Array.isArray(prev) ? prev : []
-      if (prevArray.includes(teamStr)) {
+      if (prevArray.includes(Number(teamStr))) {
         return prevArray.filter(t => t !== teamStr)
       }
       return [...prevArray, teamStr]
@@ -23,7 +23,7 @@ function TeamData() {
   }
 
   const clearAllTeams = () => {
-    setSelectedTeams([])
+    setSelectedTeams(selectedTeams)
   }
 
   const handleUseDataChange = async (scoutingId, checked) => {
